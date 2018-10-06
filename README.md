7da564a4dbae98f146fcb77cac27f8ff2fad1ffc
YanWQ-monad <YanWQmonad@gmail.com>
2018-10-06 08:16:02 +0800

    Initial project

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# MonadOJ-Frontend
+# MonadOJ
+
 Monad OJ Frontend Render
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      title: 'Monad OJ',
+      content: 'Welcome to Monad OJ'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
