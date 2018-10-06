7a3148d448af25697a795417341b27a7bb42185c
YanWQ-monad <YanWQmonad@gmail.com>
2018-10-07 03:41:17 +0800

    Write show error plugin

---
+++ src/components/ErrorShow.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="ui message"
+    :class="[level, {icon: icon}, {visible: main !== ''}]"
+  >
+    <i
+      v-if="icon !== null"
+      :class="icon"
+      class="icon"
+    />
+    <div class="content">
+      <div class="header">
+        {{ main }}
+      </div>
+      <p v-if="details instanceof String">{{ details }}</p>
+      <ul v-else-if="details instanceof Array" class="list">
+        <li v-for="detail in details" :key="detail">{{ detail }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorShow',
+  props: {
+    main: {
+      type: String,
+      default: ''
+    },
+    details: {
+      type: [ String, Array ],
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: null
+    },
+    level: {
+      type: String,
+      dafault: ''
+    }
+  }
+}
+</script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -15,36 +15,74 @@
     </div>
     <FormCacheTool v-model="form" ref="cacher" type="register" />
     <button class="ui button fluid primary" type="submit">Register</button>
+    <ErrorShow
+      :main="error.main"
+      :details="error.details"
+      icon="exclamation"
+      level="warning"
+    />
   </form>
 </template>
 
 <script>
 import FormCacheTool from '@/components/FormCacheTool.vue'
+import ErrorShow from '@/components/ErrorShow.vue'
 import { saltedPassword } from '@/utils/mappings'
 import { ajax } from '@/api'
 
+const email_re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i
+
 export default {
+  name: 'Register',
   data: () => ({
     form: {
       email: '',
       username: '',
       password: ''
+    },
+    error: {
+      main: '',
+      details: []
     }
   }),
   methods: {
     submitRegister () {
+      this.$el.querySelectorAll('.field.error')
+        .forEach(e => e.classList.remove('error'))
+      this.error.details = []
+      this.error.main = ''
+      if (!email_re.test(this.form.email.trim())) {
+        this.addErrorDetail('email', 'Email must be a valid email address')
+      }
+      if (this.form.username.trim().length === 0) {
+        this.addErrorDetail('username', 'Username could not be empty')
+      }
+      if (this.form.password.length < 8) {
+        this.addErrorDetail('password', 'Password must be at least 8 characters long')
+      }
+      if (this.error.details.length > 0) {
+        this.error.main = 'Please fix the fields error'
+        return
+      }
+
       const password = saltedPassword(this.form.password)
       ajax('/api/auth/register', 'POST', {
-        email: this.form.email,
-        username: this.form.username,
+        email: this.form.email.trim(),
+        username: this.form.username.trim(),
         password
-      }).then(data => {
+      }).then(() => {
         this.$refs.cacher.removeCache()
       })
+    },
+    addErrorDetail (field, error) {
+      this.$el.querySelectorAll('input[name="' + field + '"]')[0]
+        .parentElement.classList.add('error')
+      this.error.details.push(error)
     }
   },
   components: {
-    FormCacheTool
+    FormCacheTool,
+    ErrorShow
   }
 }
 </script>
