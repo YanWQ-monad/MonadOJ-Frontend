b739707f5f89fca13019320ea43d5eeea84e3e2b
YanWQ-monad <YanWQmonad@gmail.com>
2018-10-06 22:06:18 +0800

    Divide store into modules

---
+++ src/vuex/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import * as mutations from './mutations'
+import * as actions from './actions'
+import * as getters from './getters'
+
+import modules from './modules'
+
+Vue.use(Vuex)
+
+const state = {
+
+}
+
+const store = new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  modules: {
+    ...modules
+  },
+  state,
+  mutations,
+  actions,
+  getters
+})
+
+export default store
+
+if (module.hot) {
+  module.hot.accept([
+    './mutations',
+    './actions',
+    './getters'
+  ], () => {
+    store.hotUpdate({
+      mutations: require('./mutations'),
+      actions: require('./actions'),
+      getters: require('./getters')
+    })
+  })
+}
+
