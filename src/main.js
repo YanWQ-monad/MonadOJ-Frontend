7cab57cfa6d879db4067438cc7aea683bf2ed6e0
YanWQ-monad <YanWQmonad@gmail.com>
2018-10-14 02:49:55 +0800

    Add page permission
    - Forbid general user to access admin page
    - Forbid not-logged-in user to access login-required page

--- src/main.js
+++ src/main.js
@@ -8,11 +8,33 @@ Vue.config.productionTip = false
 import '../semantic/dist/semantic.min.css'
 import '../semantic/dist/semantic.min.js'
 
-new Vue({
+const app = new Vue({
   router,
   store,
   render: h => h(App),
   created () {
     this.$store.dispatch('auth/init')
+  },
+  mounted () {
+    if (this.$route.matched.some(record => record.meta.requiresAdmin) && store.state.auth.admin <= 0) {
+      this.$alert.addMessage('error', 'Access is denied', 'You have no permission to access the page')
+      this.$router.push({ name: 'Home' })
+    } else if (this.$route.matched.some(record => record.meta.requiresLogin) && store.state.auth.token === null) {
+      this.$alert.addMessage('info', 'Login required', 'You have to login to access the page')
+      this.$router.push({ name: 'Login' })
+    }
   }
-}).$mount('#app')
+})
+app.$mount('#app')
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAdmin) && store.state.auth.admin <= 0) {
+    app.$alert.addMessage('error', 'Access is denied', 'You have no permission to access the page')
+    next(false)
+  } else if (to.matched.some(record => record.meta.requiresLogin) && !store.getters['auth/loggedIn']) {
+    app.$alert.addMessage('info', 'Login required', 'You have to login to access the page')
+    next(false)
+  } else {
+    next()
+  }
+})
